629b8d7b6e17ddd1e14fb5cc309d1266aa95e3ab
Miles Barrios <mdb435@nau.edu>
2020-12-31 10:08:32 -0700

    Basic Styling - Tut 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import Player from './components/Player';
 import Song from './components/Song'
 
+//styles
+import './styles/app.scss';
+
 function App() {
   return (
     <div className="App">
